3499acad5c775c586f45ec5fc5a163159d4e925e
Gabrielle Rodrigues <73626380+gabrielle-1@users.noreply.github.com>
2023-03-07 21:11:20 -0300

    Create a method to calculate R2

--- src/main/java/CoeficienteCorrelacaoLinear.java
+++ src/main/java/CoeficienteCorrelacaoLinear.java
@@ -1,82 +1,54 @@
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import static java.lang.Double.NaN;
 
 public class CoeficienteCorrelacaoLinear {
 
-    public static void main(String[] args) {
+    private ArrayList<Double> xi = new ArrayList<>();
+    private ArrayList<Double> yi = new ArrayList<>();
+    private ArrayList<Double> values;
 
-        Archive archive = new Archive();
+    private ArrayList<Double> x2 = new ArrayList<>();
+    private ArrayList<Double> y2 = new ArrayList<>();
+    private ArrayList<Double> xy = new ArrayList<>();
+    int length = 0;
+    double x = 0.0, y = 0.0;
+    double sumxi = 0, sumyi = 0, sumx2 = 0, sumy2 = 0, sumxy = 0;
 
-        ArrayList<Double> xi = new ArrayList<>();
-        ArrayList<Double> yi = new ArrayList<>();
-        ArrayList<String> values;
+    public CoeficienteCorrelacaoLinear(ArrayList<Double> xi, ArrayList<Double> yi) {
+        this.xi = xi;
+        this.yi = yi;
+        this.length = xi.size();
+    }
 
-        ArrayList<Double> x2 = new ArrayList<>();
-        ArrayList<Double> y2 = new ArrayList<>();
-        ArrayList<Double> xy = new ArrayList<>();
+    public void calculate(){
+        if (this.length > 0) {
 
-        double x = 0.0, y = 0.0;
-        int sumxi = 0, sumyi = 0, sumx2 = 0, sumy2 = 0, sumxy = 0;
-
-        try {
-            values = archive.read("C:\\Users\\gabri\\OneDrive\\Documentos\\atividade-metodos-numericos\\src\\main\\resources\\file.txt");
-
-            for (String valor : values) {
-                String[] split = valor.split(" ");
-                System.out.println(values);
-
-                double valorx = Double.parseDouble(split[1].replace("," , "."));
-                double valory = Double.parseDouble(split[1].replace("," , "."));
-
-                x = valorx;
-                y = valory;
-
-                xi.add(x);
-                yi.add(y);
+            for (int i = 0; i < length; i++) {
+                x2.add(Math.pow(xi.get(i), 2));
+                y2.add(Math.pow(yi.get(i), 2));
+                xy.add(xi.get(i) * yi.get(i));
             }
 
-            int length = xi.size();
-
-            if (length > 0) {
-
-                for (int i = 0; i < length; i++) {
-                    System.out.println(xi.get(i) + " - " + yi.get(i));
-                }
-
-                for (int i = 0; i < length; i++) {
-                    x2.add(Math.pow(xi.get(i), 2));
-                    y2.add(Math.pow(yi.get(i), 2));
-                    xy.add(xi.get(i) * yi.get(i));
-                }
-
-                for (int i = 0; i < length; i++) {
-                    sumxi += xi.get(i);
-                    sumyi += yi.get(i);
-                    sumx2 += x2.get(i);
-                    sumy2 += y2.get(i);
-                    sumxy += xy.get(i);
-                }
-                
-                double angularCoefficient = calculateAngularCoefficient(length, sumxy, sumxi, sumyi, sumx2);
-                double intercept = calculateIntercept(sumyi, angularCoefficient, sumxi, length);
-                
-                calculateyM(angularCoefficient, intercept, yi, xi);      
-                
-                calculateR(length, sumxy, sumxi, sumyi, sumx2, sumy2);
-                System.out.println(showFormula(angularCoefficient, intercept));
-                System.out.println();
+            for (int i = 0; i < length; i++) {
+                sumxi += xi.get(i);
+                sumyi += yi.get(i);
+                sumx2 += x2.get(i);
+                sumy2 += y2.get(i);
+                sumxy += xy.get(i);
             }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            System.out.println("Fim do programa.");
+            double angularCoefficient = calculateAngularCoefficient(length, sumxy, sumxi, sumyi, sumx2);
+            double intercept = calculateIntercept(sumyi, angularCoefficient, sumxi, length);
+            calculateR(sumyi, angularCoefficient, intercept);
+            System.out.println(showFormula(angularCoefficient, intercept));
+            System.out.println();
         }
     }
 
-    private static double calculateAngularCoefficient (int length, double sumxy, double sumxi, double sumyi, double sumx2){
+    private double calculateAngularCoefficient (int length, double sumxy, double sumxi, double sumyi, double sumx2){
         double result = 0.0;
         if (length > 0)
             result = ((length * sumxy - (sumxi * sumyi) ) / (length * sumx2 - Math.pow(sumxi, 2)));
@@ -84,7 +56,7 @@ public class CoeficienteCorrelacaoLinear {
         return result;
     }
 
-    private static double calculateIntercept (double sumyi, double angularCoefficient, double sumxi, int length){
+    private double calculateIntercept (double sumyi, double angularCoefficient, double sumxi, int length){
         double result = 0.0;
         if (length > 0)
             result = (sumyi - angularCoefficient * sumxi) / length;
@@ -92,48 +64,35 @@ public class CoeficienteCorrelacaoLinear {
         return result;
     }
 
-    private static String showFormula (double angularCoefficient, double intercept){
+    private String showFormula (double angularCoefficient, double intercept){
         String result = "";
         if (intercept != NaN && intercept != NaN) {
-            result = "y=" + intercept + "+" + angularCoefficient + "x";
+            DecimalFormat df = new DecimalFormat("#.##");
+            result = "y=" + df.format(intercept) + "+" + df.format(angularCoefficient) + "x";
         }
         return result;
     }
 
-    private static void calculateR (int length, double sumxy, double sumxi, double sumyi, double sumx2, double sumy2){
-        int rxy = 0;
-        String result = "Rxy=";
-        if (length > 0) {
-            double up =  (length * sumxi) - (sumxi * sumyi);
-            double down = Math.sqrt(length * sumx2 - (Math.pow(sumxi, 2))) * (Math.sqrt(length * sumy2 - Math.pow(sumyi, 2)));
-            rxy = (int) (up/down);
-            if (rxy != NaN) {
-                result += rxy;
-            }
-        }
-        System.out.println(result);
-    }
-
-    private static void calculateyM(double angularCoefficient, double intercept, ArrayList<Double> yi, ArrayList<Double> xi){
-        ArrayList<Double> yM = new ArrayList<>();
-        ArrayList<Double> sumyM = new ArrayList<>();
-
-        for (int i = 0; i < yi.size(); i++) {
-            yM.add(intercept + angularCoefficient * - xi.get(i));
+    private void calculateR (double sumyi, double angularCoefficient, double intercept){
+        double result = 0;
+        double yIdeal = sumyi / this.length;
+        double sumYiLessYIdeal2 = 0.0;
+        double sumYiLessYIdeal = 0.0;
+        for (int i = 0; i < this.length; i++) {
+            double x = this.xi.get(i);
+            double y = this.yi.get(i);
+            double yiLessYIdeal = intercept + angularCoefficient * x;
+            sumYiLessYIdeal2 += Math.pow((y - yIdeal),2);
+            sumYiLessYIdeal += (y - yiLessYIdeal) * (y - yiLessYIdeal);
         }
 
-        for (int i = 0; i < yi.size(); i++) {
-            sumyM.add(Math.pow(yi.get(i) - yM.get(i), 2));
+        // Calculate R^2
+        double rSquared = 1 - (sumYiLessYIdeal / sumYiLessYIdeal2);
+
+        if (rSquared != NaN) {
+            result += rSquared;
         }
-
-        double SQR = 0.0;
-
-        
-        for (int i = 0; i < sumyM.size(); i++) {
-            SQR += sumyM.get(i);
-        }
-
-        System.out.println(SQR);
-
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("R² = " + df.format(result));
     }
 }
