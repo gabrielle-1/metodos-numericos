12ba9759d5b26429634c21f5714f29f91c294b7a
Gabrielle Rodrigues <73626380+gabrielle-1@users.noreply.github.com>
2023-03-07 21:11:45 -0300

    Create a Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Archive archive = new Archive();
+        ArrayList<String> valuesString;
+        ArrayList<Double> xi = new ArrayList<>();
+        ArrayList<Double> yi = new ArrayList<>();
+
+        try {
+            valuesString = archive.read("C:\\Users\\gabri\\OneDrive\\Documentos\\atividade-metodos-numericos\\src\\main\\resources\\file.txt");
+            for (String valor : valuesString) {
+                String[] split = valor.split(" ");
+
+                double valorx = Double.parseDouble(split[0].replace(",", "."));
+                double valory = Double.parseDouble(split[1].replace(",", "."));
+
+                xi.add(valorx);
+                yi.add(valory);
+            }
+
+            System.out.println("Valores:");
+            System.out.println(valuesString);
+
+            CoeficienteCorrelacaoLinear coeficienteCorrelacaoLinear = new CoeficienteCorrelacaoLinear(xi, yi);
+            coeficienteCorrelacaoLinear.calculate();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
